758f4698547a52239d7247b14e1c03ed55f64720
董晓勇 <2627105433@qq.com>
2021-03-05 22:16:27 +0800

    完善代码

---
+++ src/main/java/dao/UserRepository.java
@@ -0,0 +1,15 @@
+package dao;
+
+import entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Create By dongxiaoyong on /2021/3/5
+ * description: 用户dao
+ *
+ * @author dongxiaoyong
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
